2ff2947ae0b7439568ea70d1150a78d1de8b8b79
Magdiel <magdiel.altynbekov@gmail.com>
2022-11-09 00:08:38 +0600

    йцу

--- HomeWork N7 (2)/main.swift
+++ HomeWork N7 (2)/main.swift
@@ -9,3 +9,67 @@ import Foundation
 
 print("Hello, World!")
 
+extension Array{
+    
+    func square<T: Numeric>(array: [T]) -> [T]{
+        
+        var array = array
+        
+        for (index, item) in array.enumerated(){
+            array[index] = item * item
+        }
+        
+        return array
+        
+    }
+    
+}
+
+var array = [1,2,3,4,5,6,7,8,9,10]
+
+print(array.square(array: array))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//N2
+
+
+struct SearchItem <T: Equatable> {
+    let massiv: [T]
+    
+    func findElements(element: T){
+        for (index, item) in massiv.enumerated(){
+            if item == element{
+                print("Элемент найден")
+                break
+            } else if index != massiv.count - 1 {
+                continue
+            } else {
+                print("Элемент не найден")
+            }
+        }
+    }
+}
+
+
+let item = SearchItem(massiv: [5,6,3,6,8,2])
+
+item.findElements(element: 2)
+
+
+
+
